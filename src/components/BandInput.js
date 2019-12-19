cb62f9d98dccad6b5a39c97dd9537fc8ce5b4b58
noam sauer-utley <noamsauerutley@gmail.com>
2019-12-19 18:54:19 +0000

    Automatically backed up by Learn

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,10 +1,10 @@
 // Add BandInput component
 import React, { Component } from 'react'
+
+class BandInput extends Component {
 state = {
   text: ""
 }
-
-class BandInput extends Component {
   render() {
     return(
       <div>
